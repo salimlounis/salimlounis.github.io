b795f4866407917b0e7d6815ac03084956278bc7
Salim Lounis <lounissalim@gmail.com>
2024-09-02 17:35:37 -0400

    update project page to generate static params

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -2,7 +2,7 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import { Project } from "../../../src/types/Project";
-import { loadProjectById } from "../../../src/utils/loadProjects";
+import { loadProjects, loadProjectById } from "../../../src/utils/loadProjects";
 import ProjectContent from "./Project";
 
 //import { useParams } from "next/navigation";
@@ -10,6 +10,17 @@ interface ProjectPageProps {
   params: {
     id: string;
   };
+}
+
+// Function to generate static params for each project
+export async function generateStaticParams() {
+  // Load all projects to get their IDs
+  const allProjects = loadProjects("projects"); // Adjust the directory name if needed
+  const paths = allProjects.map((project) => ({
+    id: project.id,
+  }));
+
+  return paths;
 }
 
 export default async function ProjectPage({ params }: ProjectPageProps) {
